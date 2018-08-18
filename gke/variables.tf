12c4d33a25d17fbb2f5f375095f771feca93e1bc
David Rivas <david@momentlabs.io>
2018-08-17 20:52:57 -0700

    Fixed gitignore and variable syntax

--- gke/variables.tf
+++ gke/variables.tf
@@ -30,9 +30,9 @@ variable "enable_helm" {
 variable "node_permissions" {
     description = "OAuth scopes for acess to google services."
     default = [
-        "https://www.googleapis.com/auth/compute"
+        "https://www.googleapis.com/auth/compute",
         "https://www.googleapis.com/auth/devstorage.read_only",
         "https://www.googleapis.com/auth/logging.write",
-        "https://www.googleapis.com/auth/monitoring",
+        "https://www.googleapis.com/auth/monitoring"
     ]
 }
