b7012268ad7f798fefcec6c38cd760f7bfda38cc
David Rivas <david@momentlabs.io>
2018-08-17 20:22:52 -0700

    Fixed variable syntax.

--- gke/cluster.tf
+++ gke/cluster.tf
@@ -9,7 +9,7 @@ resource "google_container_cluster" "cluster" {
         labels {
             cluster = "${var.cluster_name}"
         }
-        oauth_scopes = "${node.permissions}"
+        oauth_scopes = "${var.node_permissions}"
     }
 
     # Let's set up the local kubectrl credentials for this once it's up.
